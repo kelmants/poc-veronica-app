e08cd66f0ea81ce9dc0caece1fc5b5654e478bc5
kelman celis <kelmants@gmail.com>
2020-08-03 19:51:25 -0300

    initial server

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,33 @@
-const functions = require('firebase-functions');
+var admin = require("firebase-admin");
+const functions = require("firebase-functions");
+const Service = require("./service");
+const express = require("express");
+const cors = require("cors");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const app = express();
+
+// Automatically allow cross-origin requests
+app.use(cors({ origin: true }));
+
+// Add middleware to authenticate requests
+// app.use(myMiddleware);
+
+// build multiple CRUD interfaces:
+app.get("/:id", (req, res) => {
+  res.send(Service.getById(req.params.id));
+});
+app.post("/", (req, res) => {
+  res.send(Service.create(req.body));
+});
+app.put("/:id", (req, res) =>
+  res.send(Service.update(req.params.id, req.body))
+);
+app.delete("/:id", (req, res) => {
+  res.send(Service.delete(req.params.id));
+});
+app.get("/", (req, res) => {
+  res.send(Service.list());
+});
+
+// Expose Express API as a single Cloud Function:
+exports.api = functions.https.onRequest(app);
---
+++ functions/service.js
@@ -0,0 +1,19 @@
+const Service = {
+  getById(param) {
+    return param;
+  },
+  create(body) {
+    return "param";
+  },
+  update(param, body) {
+    return param;
+  },
+  delete(param) {
+    return param;
+  },
+  list() {
+    return ["param"];
+  },
+};
+
+module.exports = Service;

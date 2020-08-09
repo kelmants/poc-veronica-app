f4ba473f58e1bd48b24ca06c862143effcfc8e18
kelman celis <kelmancelis@icloud.com>
2020-08-09 14:59:25 -0300

    chore(screens): routes's screen improvements

--- src/App.js
+++ src/App.js
@@ -7,52 +7,18 @@ import {
 } from "react-router-dom";
 import logo from "./logo.svg";
 import "./App.css";
-import NoMatch from './NoMatch/NoMatch';
-
-const Screen = () => {
-  let { id, hash } = useParams();
-  console.log(id, hash);
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-};
+import NoMatch from "./NoMatch/NoMatch";
+import { ExampleScreen } from "./components/ExampleScreen";
 
 function App() {
   return (
     <Router>
       <Switch>
         <Route exact path="/">
-          <div className="App">
-            <header className="App-header">
-              <img src={logo} className="App-logo" alt="logo" />
-              <a
-                className="App-link"
-                href="https://reactjs.org"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                init
-              </a>
-            </header>
-          </div>
+          <ExampleScreen title="Initial" />
         </Route>
         <Route path="/:id/:hash">
-          <Screen />
+          <ExampleScreen title="With param" />
         </Route>
         <Route component={NoMatch} />
       </Switch>
---
+++ src/components/ExampleScreen.js
@@ -0,0 +1,22 @@
+export const ExampleScreen = ({ title = "Nothing" }) => {
+  let { id, hash } = useParams();
+  console.log(id, hash);
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {title}
+        </a>
+      </header>
+    </div>
+  );
+};

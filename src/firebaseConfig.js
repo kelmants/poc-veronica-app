91cadfa29c8e14661b0cadffbf2470190b8d1ce9
kelman celis <kelmancelis@icloud.com>
2020-08-09 14:58:41 -0300

    feat(fireabse): add configuration

---
+++ src/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAaunczWl2G-weXbM7i3UjuOKa7_3eB3eM",
+  authDomain: "poc-veronica-app.firebaseapp.com",
+  databaseURL: "https://poc-veronica-app.firebaseio.com",
+  projectId: "poc-veronica-app",
+  storageBucket: "poc-veronica-app.appspot.com",
+  messagingSenderId: "88999204400",
+  appId: "1:88999204400:web:9ceb3218f6a403fa8afff5",
+  measurementId: "G-VLZNL65GMQ",
+};
